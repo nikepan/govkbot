308f657ddf3b17b283a6138bd57d151fa4f304f7
nikepan <nikepan@yandex.ru>
2017-01-23 22:55:41 +0300

    doc fix

--- doc.go
+++ doc.go
@@ -4,6 +4,5 @@ It includes a API for receive and send messages, chats, users info and friending
 It works simply, but need to manually get user token.
 This implementation don't use vk long pool API and proxies, and have limit 3 requests per second (VK API limit).
  */
-
 package govkbot
 
