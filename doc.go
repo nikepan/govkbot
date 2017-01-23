efdfe75351684071e1a913ea1d19582043deed35
nikepan <nikepan@yandex.ru>
2017-01-23 22:57:16 +0300

    doc fix

--- doc.go
+++ doc.go
@@ -1,7 +1,10 @@
 /*
 Package govkbot is a pure Go client library for https://vk.com messaging API.
+
 It includes a API for receive and send messages, chats, users info and friending.
+
 It works simply, but need to manually get user token.
+
 This implementation don't use vk long pool API and proxies, and have limit 3 requests per second (VK API limit).
  */
 package govkbot
