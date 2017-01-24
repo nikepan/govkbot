845cb03eecda2d6cacac1cd7a605f93618d4335b
nikepan <nikepan@yandex.ru>
2017-01-25 00:49:30 +0300

    doc fix

--- doc.go
+++ doc.go
@@ -10,10 +10,11 @@ This implementation don't use vk long pool API and proxies, and have limit 3 req
 To get token you need:
 
 1. Create app and get app_id on page https://vk.com/editapp?act=create (standalone app)
+
 2. Open in browser with logged in VK
-```
-https://oauth.vk.com/authorize?client_id={{app_id}}&scope=offline,group,messages,friends&display=page&response_type=token&redirect_uri=https://oauth.vk.com/blank.html
-```
+
+ https://oauth.vk.com/authorize?client_id={{app_id}}&scope=offline,group,messages,friends&display=page&response_type=token&redirect_uri=https://oauth.vk.com/blank.html
+
 3. Copy token query parameter from URL string
 
 */
